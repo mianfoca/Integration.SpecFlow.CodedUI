ead0ec15ffe12bdd4bb22ed1449a53d7ef4bbc49
Miguel Fonseca <mianfoca@gmail.com>
2017-04-18 14:36:33 -0500

    First Commit

---
+++ CodedUITests/Features/CalculatorFeature.feature
@@ -0,0 +1,19 @@
+﻿Feature: CalculatorFeature
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario Outline: Add two numbers
+	Given I have entered <number_1> into the calculator
+	And I press add
+	And I have entered <number_2> into the calculator
+	When I press equals
+	Then the result should be <result> on the screen
+
+	Examples: 
+	| number_1 | number_2 | result |
+	| 1        | 2        | 3      |
+	| 10       | 20       | 30     |
+	| 50       | 35       | 85     |
+	| 24567    | 12345    | 36912  |
---
+++ CodedUITests/Steps/CalculatorSteps.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Calculator.Test.Steps
+{
+    [Binding]
+    public sealed class CalculatorSteps
+    {
+        UITestControl calcWindow;
+
+        [BeforeScenario]
+        public void Initialize()
+        {
+            Console.WriteLine("Inicializando");
+            ApplicationUnderTest.Launch(@"C:\Windows\System32\calc.exe");
+
+            calcWindow = new UITestControl();
+            calcWindow.TechnologyName = "MSAA";
+            calcWindow.SearchProperties[UITestControl.PropertyNames.Name] = "Calculadora";
+            calcWindow.SearchProperties[UITestControl.PropertyNames.ClassName] = "CalcFrame";
+        }
+
+        [Given("I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredSomethingIntoTheCalculator(int number)
+        {
+            Console.WriteLine("Given a number: " + number);
+
+            UITestControl btnNumber = new WinButton(calcWindow);
+            number.ToString().ToList().ForEach(n =>
+            {
+                btnNumber.SearchProperties[WinButton.PropertyNames.Name] = n.ToString();
+                Mouse.Click(btnNumber);
+            });
+        }
+
+        [Given("I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("When I press add");
+
+            UITestControl btnAdd = new WinButton(calcWindow);
+            btnAdd.SearchProperties[WinButton.PropertyNames.Name] = "Sumar";
+            Mouse.Click(btnAdd);
+        }
+
+        [When("I press equals")]
+        public void WhenIPressEquals()
+        {
+            Console.WriteLine("When I press equals");
+
+            UITestControl btnAdd = new WinButton(calcWindow);
+            btnAdd.SearchProperties[WinButton.PropertyNames.Name] = "Igual a";
+            Mouse.Click(btnAdd);
+        }
+
+        [Then("the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBe(int resultExpect)
+        {
+            Console.WriteLine("Then I should see: " + resultExpect);
+
+            WinText txtResultado = new WinText(calcWindow);
+            txtResultado.SearchProperties[WinButton.PropertyNames.Name] = "Resultado";
+
+            Assert.AreEqual(resultExpect.ToString(), txtResultado.DisplayText);
+        }
+    }
+}
